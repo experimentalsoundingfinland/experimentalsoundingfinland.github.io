d4e8ad641a23dc5553af139773db03e49710bfd7
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-04-26 21:27:03 +0200

    Update script_02.js

--- script_02.js
+++ script_02.js
@@ -24,6 +24,7 @@ async function fetchUpcomingEvents() {
 
                 row.appendChild(summaryCell);
                 row.appendChild(dateCell);
+                row.appendChild(descriptionCell);
                 row.appendChild(locationCell);
                 eventsList.appendChild(row);
             }
