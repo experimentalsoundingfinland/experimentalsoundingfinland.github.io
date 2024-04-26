3c38c0d1b4b2ae524012f3f6e733fbd7c1b063d8
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-04-27 01:32:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function fetchUpcomingEvents() {
                 const summaryCell = document.createElement('td');
                 const descriptionCell = document.createElement('td');
 
-                dateLocationCell.innerHTML = `${formatDate(eventDateTime)}<br/><a href="https://www.google.com/maps/place/${encodeURI(event.location)}" target="_blank">${event.location}</a>`; // Format date and location
+                dateLocationCell.innerHTML = `<strong>${formatDate(eventDateTime).split(' ')[0]}</strong> ${formatDate(eventDateTime).split(' ')[1]}<br/><a href="https://www.google.com/maps/place/${encodeURI(event.location)}" target="_blank">${event.location}</a>`; // Format date and location
                 summaryCell.textContent = event.summary;
                 descriptionCell.innerHTML = event.description || 'No description available';
 
