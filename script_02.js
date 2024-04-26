32f8d5ee7e47876d1b229ece5093f18b6add6abd
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-04-26 21:19:20 +0200

    Update script_02.js

--- script_02.js
+++ script_02.js
@@ -1,6 +1,6 @@
 async function fetchUpcomingEvents() {
     try {
-        const response = await fetch('https://www.googleapis.com/calendar/v3/calendars/{experimentalsoundingfinland@gmail.com}/events?key=AIzaSyA8ibG6fO1SGlZilUaFrtQ-oFg0fQF2ksg');
+        const response = await fetch('https://www.googleapis.com/calendar/v3/calendars/experimentalsoundingfinland@gmail.com/events?key=AIzaSyA8ibG6fO1SGlZilUaFrtQ-oFg0fQF2ksg');
         const data = await response.json();
         const eventsList = document.getElementById('events-list');
 
