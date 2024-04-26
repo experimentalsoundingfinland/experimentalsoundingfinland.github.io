6014d4b87770643422daab15c6b68b1d01bea4bb
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-04-27 01:47:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,5 +40,5 @@ fetchUpcomingEvents();
 // Function to format date as "YYYY-MM-DD HH:MM"
 function formatDate(date) {
     const options = { year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
-    return date.toLocaleString('fi-FI', options);
+    return date.toLocaleString('en-GB', options);
 }
