43b3deecd094ec172312791f2d110f85e16fb47b
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-04-28 13:35:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 function linkify(inputText) {
     let replacedText, replacePattern1, replacePattern2, replacePattern3;
 
-    //URLs starting with http://, https://, or ftp://
-    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+    // Split the input text into an array of strings using <br/> as the separator
+    const inputTextArray = inputText.split('<br/>');
 
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+    // Apply the linkify function to each string in the array
+    const replacedTextArray = inputTextArray.map((text) => {
+        //URLs starting with http://, https://, or ftp://
+        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+        replacedText = text.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
-    //Change email addresses to mailto:: links.
-    replacePattern3 = /(([a-zA-Z0-9\-.])+@[a-zA-Z0-9\-.]+\.[a-zA-Z0-9]{2,5})/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
-    return replacedText;
+        //Change email addresses to mailto:: links.
+        replacePattern3 = /(([a-zA-Z0-9\-.])+@[a-zA-Z0-9\-.]+\.[a-zA-Z0-9]{2,5})/gim;
+        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+        return replacedText;
+    });
+
+    // Join the array of strings back into a single string, using <br/> as the separator
+    return replacedTextArray.join('<br/>');
 }
 
 async function fetchUpcomingEvents() {
