94f383ee7c5925a7be01de781810c72e1672261b
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-04-29 17:03:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 function linkify(inputText) {
     let replacedText, replacePattern1, replacePattern2, replacePattern3;
 
-    // Split the input text into an array of strings using <br/> as the separator
     const inputTextArray = inputText.split('<br/>');
 
-    // Apply the linkify function to each string in the array
     const replacedTextArray = inputTextArray.map((text) => {
-        //URLs starting with http://, https://, or ftp://
         replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
         replacedText = text.replace(replacePattern1, '<a href="$1" target="_blank" class="url">$1</a>');
 
-        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
         replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
         replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank" class="url">$2</a>');
 
-        //Change email addresses to mailto:: links.
         replacePattern3 = /(([a-zA-Z0-9\-.])+@[a-zA-Z0-9\-.]+\.[a-zA-Z0-9]{2,5})/gim;
         replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
 
         return replacedText;
     });
 
-    // Join the array of strings back into a single string, using <br/> as the separator
     return replacedTextArray.join('<br/>');
 }
 
@@ -31,40 +25,40 @@ async function fetchUpcomingEvents() {
         const data = await response.json();
         const eventsList = document.getElementById('events-list');
 
-        const now = new Date(); // Get the current date and time
+        const now = new Date();
 
-        // Sort the items array by date
         data.items.sort((a, b) => new Date(a.start.dateTime || a.start.date) - new Date(b.start.dateTime || b.start.date));
 
         data.items.forEach((event) => {
             const eventStartDateTime = new Date(event.start.dateTime || event.start.date);
             const eventEndDateTime = new Date(event.end.dateTime || event.end.date);
 
-            // Display events that have started but not yet finished, and future events
             if (eventEndDateTime > now) {
                 const row = document.createElement('tr');
-                const dateLocationCell = document.createElement('td'); // New cell for date and location
-                const summaryDescriptionCell = document.createElement('td'); // New cell for summary and description
+                const dateLocationCell = document.createElement('td');
+                const summaryDescriptionCell = document.createElement('td');
 
-                // Check if location is defined, if not, set it to 'Location missing'
                 const location = event.location || 'Location missing';
 
-                // Extract the venue from the event description and remove it from the description
                 const venuePrefix = 'event_venue123 ';
                 let venue = 'Venue missing';
                 let description = event.description || 'No description available';
                 if (description.includes(venuePrefix)) {
                     const splitDescription = description.split(venuePrefix);
-                    venue = splitDescription[1]; // Take everything after the prefix as the venue
+                    venue = splitDescription[1];
                     description = splitDescription[0];
                 }
 
-                dateLocationCell.innerHTML = `<strong>${formatDate(eventStartDateTime).split('<br/>')[0]}</strong><br/>${formatDate(eventStartDateTime).split('<br/>')[1]}<br/><strong>${venue}</strong><br/><a href="https://www.google.com/maps/place/${encodeURIComponent(location)}" target="_blank">${location}</a>`; // Format date, venue, and location
-                summaryDescriptionCell.innerHTML = `<strong>${event.summary}</strong><br/>${linkify(description.replace(/\n/g, '<br/>'))}`; // Format summary and description
+                dateLocationCell.innerHTML = `<strong>${formatDate(eventStartDateTime).split('<br/>')[0]}</strong><br/>${formatDate(eventStartDateTime).split('<br/>')[1]}<br/><strong>${venue}</strong><br/><a href="https://www.google.com/maps/place/${encodeURIComponent(location)}" target="_blank">${location}</a>`;
+                summaryDescriptionCell.innerHTML = `<strong>${event.summary}</strong><br/>${linkify(description.replace(/\n/g, '<br/>'))}`;
 
                 row.appendChild(dateLocationCell);
                 row.appendChild(summaryDescriptionCell);
                 eventsList.appendChild(row);
+
+                const spacerRow = document.createElement('tr');
+                spacerRow.className = 'spacer';
+                eventsList.appendChild(spacerRow);
             }
         });
     } catch (error) {
@@ -72,20 +66,17 @@ async function fetchUpcomingEvents() {
     }
 }
 
-// Call the function to fetch and display upcoming events
 fetchUpcomingEvents();
 
-// Function to format date as "dd.mm.yy", "weekday", and "hh:mm"
 function formatDate(date) {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const day = date.getDate();
-    const month = date.getMonth() + 1; // Months are zero-based in JavaScript
+    const month = date.getMonth() + 1;
     const year = date.getFullYear().toString().substr(-2);
     const weekday = days[date.getDay()];
     const hours = date.getHours();
     const minutes = date.getMinutes();
 
-    // Ensure all values are defined before returning
     if(day && month && year && weekday && hours != null && minutes != null) {
         return `<strong>${day}.${month < 10 ? '0' : ''}${month}.${year}</strong><br/>${weekday}, ${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
     } else {
