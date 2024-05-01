186a4c56211e572dc65bfeef4ac68b31f619102e
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-05-01 12:58:44 +0200

    Update colorTitle.js

--- assets/colorTitle.js
+++ assets/colorTitle.js
@@ -3,18 +3,23 @@ window.onload = function() {
   var words = title.textContent.split(' ');
   var newTitle = '';
 
+  var minChange = 2; // Minimum number of letters to change
+  var maxChange = 5; // Maximum number of letters to change
+
   for (var i = 0; i < 2; i++) {
     var word = words[i];
+    var changeCount = Math.floor(Math.random() * (maxChange - minChange + 1)) + minChange; // Number of letters to change in the current word
+
     for (var j = 0; j < word.length; j++) {
-      var color;
-      do {
-        color = Math.floor(Math.random()*16777215).toString(16);
-      } while (parseInt(color, 16) > 16777215 * 0.1 || // Avoid colors that are too light
-               color > 'aaff00' && color < 'bfff00' || // Avoid light green
-               color > 'ffffaa' && color < 'ffffbf');  // Avoid light yellow
-      // Change the color of only a few random letters
-      if (Math.random() < 0.5) {
+      if (changeCount > 0) {
+        var color;
+        do {
+          color = Math.floor(Math.random()*16777215).toString(16);
+        } while (parseInt(color, 16) > 16777215 * 0.1 || // Avoid colors that are too light
+                 color > 'aaff00' && color < 'bfff00' || // Avoid light green
+                 color > 'ffffaa' && color < 'ffffbf');  // Avoid light yellow
         newTitle += '<span style="color: #' + color + ';">' + word[j] + '</span>';
+        changeCount--;
       } else {
         newTitle += word[j];
       }
