8aa21a1e17695e36c4d248ea9647edd35f4f3cbb
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-05-01 13:14:54 +0200

    Update colorTitle.js

--- assets/colorTitle.js
+++ assets/colorTitle.js
@@ -6,18 +6,18 @@ window.onload = function() {
   var minChange = 2; // Minimum number of letters to change
   var maxChange = 5; // Maximum number of letters to change
 
+  // Array of colors to use
+  var colors = ['#000000', '#003300', '#000033', '#330000', '#333333', '#003333', '#330033', '#333300', '#333366', '#006600', '#000066', '#660000'];
+
   for (var i = 0; i < 2; i++) {
     var word = words[i];
     var changeCount = Math.floor(Math.random() * (maxChange - minChange + 1)) + minChange; // Number of letters to change in the current word
 
     for (var j = 0; j < word.length; j++) {
       if (changeCount > 0) {
-        var color;
-        do {
-          color = Math.floor(Math.random()*0x666666).toString(16);
-        } while (color > 'aaff00' && color < 'bfff00' || // Avoid light green
-                 color > 'ffffaa' && color < 'ffffbf');  // Avoid light yellow
-        newTitle += '<span style="color: #' + color + ';">' + word[j] + '</span>';
+        // Select a random color from the array
+        var color = colors[Math.floor(Math.random() * colors.length)];
+        newTitle += '<span style="color: ' + color + ';">' + word[j] + '</span>';
         changeCount--;
       } else {
         newTitle += word[j];
