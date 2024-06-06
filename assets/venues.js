4819da4b0b4fcff6927af492118ee3b8b8a78a3f
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-06-06 18:24:35 +0200

    Update venues.js

--- assets/venues.js
+++ assets/venues.js
@@ -1,31 +1,25 @@
-// Include the Papa Parse library
-<script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.0/papaparse.min.js"></script>
-
-// Your JavaScript code
-<script>
+// Your Google Sheet ID
 const googleSheetID = '2PACX-1vQwM9YFlWmK-XWUqjgqI9h8gEaC3gSB-zFfx91BlBPnFRjVqGMe7sBVrqJmGonki8MBDI4Pw7LshgJ6';
+
+// The URL to your published Google Sheet in CSV format
 const url = `https://docs.google.com/spreadsheets/d/${googleSheetID}/gviz/tq?tqx=out:csv`;
 
+// Fetch the CSV data from the Google Sheet
 fetch(url)
   .then(response => response.text())
   .then(data => {
+    // Parse the CSV data to JSON
     const results = Papa.parse(data, {header: true, dynamicTyping: true}).data;
+
     let html = '<table>';
 
     // Add table headers
     html += '<tr><th>Aikaleima</th><th>Name of the venue</th><th>Address</th><th>Description</th><th>Technical information</th><th>Contacts</th><th>Not a robot?</th></tr>';
 
+    // Loop through each row in the data
     results.forEach(row => {
-      const aikaleima = row['Aikaleima'];
-      const name = row['Name of the venue'];
-      const address = row['Address'];
-      const description = row['Description'];
-      const technicalInfo = row['Technical information'];
-      const contacts = row['Contacts'];
-      const notARobot = row['Not a robot?'];
-
       // Add a new row to the table for each entry
-      html += `<tr><td>${aikaleima}</td><td>${name}</td><td>${address}</td><td>${description}</td><td>${technicalInfo}</td><td>${contacts}</td><td>${notARobot}</td></tr>`;
+      html += `<tr><td>${row['Aikaleima']}</td><td>${row['Name of the venue']}</td><td>${row['Address']}</td><td>${row['Description']}</td><td>${row['Technical information']}</td><td>${row['Contacts']}</td><td>${row['Not a robot?']}</td></tr>`;
     });
 
     html += '</table>';
@@ -33,4 +27,3 @@ fetch(url)
     // Add the table to the body of your HTML
     document.body.innerHTML = html;
   });
-</script>
