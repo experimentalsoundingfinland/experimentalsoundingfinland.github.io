3f3731594c67eaa2f9eb5c17c7b192f100188d76
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-06-06 18:45:06 +0200

    Update venues.js

--- assets/venues.js
+++ assets/venues.js
@@ -11,12 +11,12 @@ fetch(url)
     let html = '<table>';
 
     // Add table headers
-    html += '<tr><th>Aikaleima</th><th>Name of the venue</th><th>Address</th><th>Description</th><th>Technical information</th><th>Contacts</th><th>Not a robot?</th></tr>';
+    html += '<tr><th>Name of the venue</th><th>Address</th><th>Description</th><th>Technical information</th><th>Contacts</th></tr>';
 
     // Loop through each row in the data
     results.forEach(row => {
       // Add a new row to the table for each entry
-      html += `<tr><td>${row['Aikaleima']}</td><td>${row['Name of the venue']}</td><td>${row['Address']}</td><td>${row['Description']}</td><td>${row['Technical information']}</td><td>${row['Contacts']}</td><td>${row['Not a robot?']}</td></tr>`;
+      html += `<tr><td>${row['Name of the venue']}</td><td>${row['Address']}</td><td>${row['Description']}</td><td>${row['Technical information']}</td><td>${row['Contacts']}</td></tr>`;
     });
 
     html += '</table>';
