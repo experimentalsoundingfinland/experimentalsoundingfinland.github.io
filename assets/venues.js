899fb971ad56b6b652e8fd90a615a64b0c50efae
experimentalsoundingfinland <168072676+experimentalsoundingfinland@users.noreply.github.com>
2024-06-06 17:12:50 +0200

    Update venues.js

--- assets/venues.js
+++ assets/venues.js
@@ -1,5 +1,5 @@
 const googleSheetID = '2PACX-1vQwM9YFlWmK-XWUqjgqI9h8gEaC3gSB-zFfx91BlBPnFRjVqGMe7sBVrqJmGonki8MBDI4Pw7LshgJ6';
-const url = `https://docs.google.com/spreadsheets/feeds/list/${googleSheetID}/od6/public/values?alt=json`;
+const url = `https://spreadsheets.google.com/feeds/list/${googleSheetID}/od6/public/values?alt=json`;
 
 fetch(url)
   .then(response => response.json())
